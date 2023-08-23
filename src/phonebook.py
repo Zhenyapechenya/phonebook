5f009a81e1f710e0341ffa821fd791742fa5d5e6
Zhenyapechenya <epsareva77@gmail.com>
2023-08-23 20:14:08 +0700

    in process

--- src/phonebook.py
+++ src/phonebook.py
@@ -3,8 +3,9 @@ from sys import exit
 
 class Phonebook:
 
+    FILE_NAME = "source.txt"
+
     def get_phonebook_from_file(self, filename: str) -> list:
-        # ТУТ ДОЛЖЕН БЫТЬ СПИСОК СПИСКОВ
         try:
             with open(filename, "r+", encoding="utf-8") as file:
                 result = [str.strip("\n").split("*") for str in file.readlines() if str != "\n"]
@@ -12,33 +13,49 @@ class Phonebook:
         except FileNotFoundError as e:
             print(e)
 
-    def print_phonebook(self, phonebook_list: list) -> None:
+
+    def print_phonebook(self) -> None:
+        phonebook_list = self.get_phonebook_from_file(self.FILE_NAME)
         # НЕ ЗАБУДЬ ПЕРЕНЕСТИ ДЛИННУЮ СТРОКУ
-        print("\n№   | Имя              | Фамилия                 | Отчество                 | Название организации   | Рабочий телефон   | Личный телефон")
+        print("\n№   | Имя              | Фамилия                 | Отчество                 | Название организации        | Рабочий телефон   | Личный телефон")
         # print("\033[H\033[J")
         for note in phonebook_list:
             number = note[0]
             name = note[1]
             surname = note[2]
             patronymic = note[3]
+            organization = note[4]
+            work_phone = note[5]
+            cellphone = note[6]
             print("{:-<150}".format("-"))
-            print("{:<6}{:<19}{:<26}{:<26}".format(number, name, surname, patronymic))
+            print("{:<6}{:<19}{:<26}{:<27}{:<30}{:<20}{:<20}".format(number, name, surname, patronymic, organization, work_phone, cellphone))
+
+    def add_new_note(self, filename: str) -> None:
+        name = input("Введите имя: ")
+        surname = input("Введите фамилию: ")
+        patronymic = input("Введите отчество: ")
+        organization = input("Введите название организации: ")
+        work_phone = input("Введите рабочий телефон: ")
+        cellphone = input("Введите личный (сотовый) телефон: ")
+        number = len(self.get_phonebook_from_file(self.FILE_NAME)) + 1
+        new_note = [str(number), name, surname, patronymic, organization, work_phone, cellphone]
+        new_note = "*".join(new_note)
+        try:
+            with open(filename, "a", encoding="utf-8") as file:
+                # тут полученные данные должны сохраняться в список,
+                # соединяются звездочкой и записываются в файл
+                file.write(new_note + "\n")
+        except FileNotFoundError as e:
+            print(e)
+
+    #     НУЖНА ФУНКЦИЯ ОБНОВЛЕНИЯ СПИСКА, ПОЛУЧЕННОГО ИЗ ФАЙЛА
 
 
 one = Phonebook()
-res = one.get_phonebook_from_file("source.txt")
+res = one.get_phonebook_from_file(one.FILE_NAME)
 
 
-def add_new_note(filename: str) -> None:
-    try:
-        with open(filename, "a", encoding="utf-8") as file:
-            # тут полученные данные должны сохраняться в список,
-            # соединяются звездочкой и записываются в файл
-            file.write("check"+"\n")
-    except FileNotFoundError as e:
-        print(e)
 
-#     НУЖНА ФУНКЦИЯ ОБНОВЛЕНИЯ СПИСКА, ПОЛУЧЕННОГО ИЗ ФАЙЛА
 
 def main_menu(prev_param: int = 0) -> None:
     param = input(
@@ -50,9 +67,9 @@ def main_menu(prev_param: int = 0) -> None:
     """)
 
     if param == "1":
-        one.print_phonebook(res)
+        one.print_phonebook()
     elif param == "2":
-        add_new_note("source.txt")
+        one.add_new_note(one.FILE_NAME)
         print("Запись добавлена в справочник.")
     elif param == "3":
         print("тут будет функция редактирования записи")
